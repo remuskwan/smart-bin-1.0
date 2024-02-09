91464649fe4c48523f3551fd53fe4142bc5a5cf2
Remus <remuskwan23@gmail.com>
2024-02-10 00:00:50 +0800

    init: create next app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { trpc } from "@/utils/trpc";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const hello = trpc.hello.useQuery({ text: "client" });
+  if (!hello.data) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
