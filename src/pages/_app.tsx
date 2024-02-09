fff587888ebcc8030a7552b391bd662fa96bd01f
Remus <remuskwan23@gmail.com>
2024-02-10 00:12:07 +0800

    build: fix imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
