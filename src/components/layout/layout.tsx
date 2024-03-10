10d87aa8938757c7e4983f8c402c94170ad5a6fb
keithcharlesc <keithcharleschan@gmail.com>
2024-03-10 23:27:56 +0800

    Added Layout Component, Index Page with Icon

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from "react";
+import logo from "@/assets/logo.png";
+import Image from "next/image";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="relative flex flex-col items-center justify-center min-h-screen bg-[#F4EDCC]">
+      <div className="absolute top-10 flex items-center">
+        <h1 className="text-9xl font-bold">SortWise</h1>
+        <Image src={logo} alt="logo" className="h-24 w-24" />
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,22 @@ import "@/styles/globals.css";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
 import { Inter } from "next/font/google";
+import Head from "next/head";
+import logo from "@/assets/logo.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <main className={inter.className}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <link rel="icon" href={logo.src} type="image/png" />
+        <title>SortWise</title>
+      </Head>
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 };
 
