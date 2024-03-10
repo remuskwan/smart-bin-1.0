1c70410276aa441deabb7cdcfad924dd3111774a
keithcharlesc <keithcharleschan@gmail.com>
2024-03-11 01:56:47 +0800

    Added animation for Recycle Item Button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,10 +16,21 @@ export default function Home() {
 
   return (
     <Layout>
+      <style jsx>{`
+        @keyframes bounce {
+          0%,
+          100% {
+            transform: translateY(0);
+          }
+          50% {
+            transform: translateY(-1rem);
+          }
+        }
+      `}</style>
       <button
         onClick={handleRecycleClick}
         type="button"
-        className="bg-green-500 text-white text-9xl px-20 py-10 inline-block text-center rounded-lg transition duration-500 ease-in-out transform hover:-translate-y-1 active:scale-90"
+        className="bg-green-500 text-white text-9xl px-20 py-10 inline-block text-center rounded-lg transition duration-500 ease-in-out transform hover:animate-bounce active:scale-90"
       >
         Recycle Item
       </button>
