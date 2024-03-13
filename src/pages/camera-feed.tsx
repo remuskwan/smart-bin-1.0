b147e00cbe7a2df3e79f5c33027078f935885e4d
keithcharlesc <keithcharleschan@gmail.com>
2024-03-13 20:43:41 +0800

    Camera feed to send photo to AWS

--- src/pages/camera-feed.tsx
+++ src/pages/camera-feed.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useRef, useState } from "react";
 
-const CameraFeed: React.FC = () => {
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [image, setImage] = useState<string | null>(null);
+const CameraFeed = () => {
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [image, setImage] = useState(null);
 
   useEffect(() => {
     const getVideo = async () => {
@@ -22,6 +22,23 @@ const CameraFeed: React.FC = () => {
     getVideo();
   }, []);
 
+  const uploadImage = async (imageBlob) => {
+    try {
+      const response = await fetch('https://ittyekb4bb.execute-api.ap-southeast-1.amazonaws.com/dev/sortwise-new-images/image.png', {
+        method: 'PUT',
+        body: imageBlob,
+        headers: {
+          'Content-Type': 'image/png' // or the specific type of your image
+        }
+      });
+      const data = await response;
+      console.log('File upload response:', data);
+    } catch (error) {
+      console.error('Error uploading file:', error);
+    }
+  };
+  
+
   const takePhoto = () => {
     if (videoRef.current && canvasRef.current) {
       const context = canvasRef.current.getContext("2d");
@@ -31,9 +48,11 @@ const CameraFeed: React.FC = () => {
         canvasRef.current.height = videoHeight;
         context.drawImage(videoRef.current, 0, 0, videoWidth, videoHeight);
 
-        // Convert canvas to image
-        const imageDataUrl = canvasRef.current.toDataURL("image/png");
-        setImage(imageDataUrl);
+        // Convert canvas to image and then to blob
+        canvasRef.current.toBlob((blob) => {
+          setImage(URL.createObjectURL(blob));
+          uploadImage(blob);
+        }, 'image/png');
       }
     }
   };
